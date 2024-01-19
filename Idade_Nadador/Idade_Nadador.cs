ed5c82b3e27c35f5be4ba17f85a32087e6db1ba9
Joana Luiza <joanasilva@uniaraxa.edu.br>
2024-01-19 08:50:07 -0300

    Primeiro commit

---
+++ Idade_Nadador/Idade_Nadador.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade_Nadador
+{
+    internal class Idade_Nadador
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Denovo();
+                Console.WriteLine("Deseja verificar a idade de outro nadador? (Digite 's' para sim, ou qualquer outra tecla para sair)");
+            } while (Console.ReadLine().ToLower() == "s");
+        }
+        public static void Denovo()
+        {
+            int Idade;
+
+            Console.WriteLine("Digite a idade do nadador:");
+            Idade = int.Parse(Console.ReadLine());
+
+            if (Idade >= 5 && Idade <= 7)
+            {
+                Console.Write("O nadador e do grupo: \t");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Infantil A \n");
+                Console.ResetColor();
+            }
+            if (Idade >= 8 && Idade <= 11)
+            {
+                Console.Write("O nadador e do grupo: \t");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Infaltil B \n");
+                Console.ResetColor();
+            }
+            if (Idade == 12 || Idade == 13)
+            {
+                Console.Write("O nadador e do grupo: \t");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Juvenil A \n");
+                Console.ResetColor();
+            }
+            if (Idade >= 14 && Idade <= 17)
+            {
+                Console.Write("O nadador e do grupo: \t");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Juvenil B \n");
+                Console.ResetColor();
+            }
+            if (Idade >= 18)
+            {
+                Console.Write("O nadador e do grupo: \t");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Adultos \n");
+                Console.ResetColor();
+            }
+
+
+        }
+    }
+}
